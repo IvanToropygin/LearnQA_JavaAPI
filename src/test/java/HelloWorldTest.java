45dabe0aa327e351be7d9d5227d2dd1b39d1d411
IvanToropygin <itori86@mail.ru>
2022-12-28 21:00:30 +0300

    HelloFromIvan

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,19 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
     @Test
     public void testHelloWorld(){
-        System.out.println("Hello world!");
+        Response response = RestAssured
+                .get("http://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
+    }
+
+    @Test
+    public void testHelloFromIvan(){
+        System.out.println("Hello from Ivan");
     }
 }
