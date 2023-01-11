fb0ee41c61cb0f4bf07e340fc2964ba3bd922aaa
IvanToropygin <itori86@mail.ru>
2023-01-11 21:17:26 +0300

    Ex12: Тест запроса на метод header

---
+++ src/test/java/tests/EX12.java
@@ -0,0 +1,27 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class EX12 {
+
+    @Test
+    public void testHeaderMethod(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_header")
+                .andReturn();
+        Headers headers = response.getHeaders();
+
+        Assertions.assertAll(
+                () -> assertTrue(headers.hasHeaderWithName("x-secret-homework-header"),
+                        "headers doesn't contains secret header"),
+                () -> assertTrue(headers.getValue("x-secret-homework-header").equals("Some secret value"),
+                        "cookie with name 'Some secret value' doesn't contains value 'Some secret value'")
+        );
+    }
+}
